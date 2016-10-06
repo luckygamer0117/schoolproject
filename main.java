8a2088b035e26d5a7572e4a945a0f21804ab07f3
luckygamer0117 <jchen1796@gmail.com>
2016-10-06 17:41:14 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,38 @@
+
+public class main {
+  
+  public static void main (String arg[]) {
+  
+   rocketTop();
+   rocketLine();
+   rocketMid();
+   rocketMid();
+   rocketLine();
+   rocketWord();
+   rocketWordtwo();
+   rocketLine();
+   rocketMid();
+   rocketMid();
+   rocketLine();
+   rocketTop();
+  }
+  
+  public static void rocketTop () {
+  	System.out.println ("   /\\	    /\\ ");
+    System.out.println ("  /  \\	   /  \\ ");
+    System.out.println (" /    \\	  /    \\  ");
+  }
+  public static void rocketMid () {
+	System.out.println ("|      | |      |");
+  }
+  public static void rocketLine () {
+    System.out.println ("+------+ +------+");
+  }
+  public static void rocketWord () {
+      System.out.println ("|United| |United|");
+    }
+  public static void rocketWordtwo () {
+    	System.out.println ("|States| |States|");
+   
+	}
+}
